186675c2112dfb70503e8c1ff60a3ff8b8e8e57e
bohlscf <44482641+bohlscf@users.noreply.github.com>
2019-02-12 17:11:22 -0500

    Add files via upload

---
+++ SOS_Light1/SOS_Light1.ino
@@ -0,0 +1,31 @@
+// This program lights and blinks an LED
+int ledPin12 = 12;
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (i % 9 >= 3 && i % 9 <= 5){
+    digitalWrite(ledPin12, HIGH);
+    delay(500);
+    digitalWrite(ledPin12, LOW);
+    delay(500);
+  }
+  else{
+    digitalWrite(ledPin12, HIGH);
+    delay(250);
+    digitalWrite(ledPin12, LOW);
+    delay(250);
+    if(i % 9 == 8){
+      delay(800);
+    }
+    if(i % 9 == 2){
+      delay(250);
+    }
+  }
+  i++;
+}
